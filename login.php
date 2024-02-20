edc23d525b3c1b64609684c4c3183091eb4efaeb
karissa-plathe <71577471+karissa-plathe@users.noreply.github.com>
2024-02-20 11:15:20 -0600

    initial
    login page created

---
+++ login.php
@@ -0,0 +1,82 @@
+<html>
+ 
+    <head>
+        <link rel="stylesheet" href="style.css">
+        <title>Login</title>
+    </head>
+
+    <body id ="blur">
+        <div>
+            <div id ="clear">
+                <h1>Login</h1>
+                <form method=POST>
+                    <div>
+                        <input id="user" type="text" name="user" placeholder="Username">
+                    </div>
+                    <div>
+                        <input id="pass" type="password" name="pass" placeholder="Password">
+                    </div>
+                    <button name="sub" type="submit">Login</button>
+                    <button name="submitNew" type="submit">Guest</button>
+                </form>
+            </div>
+        </div>
+
+        <?php
+            // Report all error information on the webpage
+            error_reporting(E_ALL);
+            ini_set('display_errors', 1);
+            
+            // submitting new user and pass
+            if (isset($_POST["submitNew"])) {
+                // go to the guest page
+            }
+        
+            if (isset($_POST['sub'])) {
+                $servername = "localhost";
+                $username = "admin"; 
+                $password = "admin"; 
+                $dbname = "dbname";
+        
+                // Create connection
+                $conn = new mysqli($servername, $username, $password, $dbname);
+                $check = 0;
+
+                // Check connection
+                if ($conn->connect_error) {
+                    die("Connection failed: " . $conn->connect_error);
+                }
+        
+                // Use prepared statement in SQL
+                $name = $_POST['user'];
+                $pass = $_POST['pass'];
+                $sql = "SELECT * FROM login";
+        
+                $result = $conn->query($sql);
+                if ($result->num_rows > 0) {
+                    // output data of each row
+                    while ($row = $result->fetch_assoc()) { // info about query"https://www.w3schools.com/php/php_mysql_select.asp
+                        if ($row["username"] == $name) {
+                            $check = 1;
+                            if ($row["password"] != $pass) {
+                                echo '<script>alert("Username does not match password")</script>';
+                            } else {
+                                echo "connected";
+                                session_start();
+                                $_SESSION["Username"] = $name;
+                                header("Location: decks.php");
+                                exit();
+                            }
+                        }
+                    }
+                    if ($check == 0) {
+                        echo '<script>alert("Username does not exist")</script>';
+                    }
+                }
+                $result->close();
+                $conn->close();
+            }
+        ?>
+
+    </body>
+</html>
